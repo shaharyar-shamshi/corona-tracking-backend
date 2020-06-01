feb22961d9ba80e6f39f8b297065a26e9a538ac7
pkpsyder007 <pkpsyder007@gmail.com>
2020-06-01 21:26:00 +0530

    Fixing file structure

--- app.js
+++ app.js
@@ -1,30 +1,16 @@
 const express = require("express");
 const app = express();
-var router = express.Router();
-const getBingData = require('./getBingData')
-const getHistoricalData = require('./getHistoricalData')
-const bodyParser = require('body-parser');
-const port = process.env.PORT || 3000
+const bodyParser = require("body-parser");
+const router = require("./routes");
+const middlewares = require("./utils/middlewares");
 
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true})); 
-app.use(bodyParser.json({limit: '50mb'}));
+// Initialize middlewares
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(middlewares.setHeaders);
 
-app.use(function (req, res, next) { 
-// Website you wish to allow to connect 
-res.setHeader('Access-Control-Allow-Origin', '*'); 
-// Request methods you wish to allow 
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE'); 
-// Request headers you wish to allow 
-res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization'); 
-// Set to true if you need the website to include cookies in the requests sent 
-// to the API (e.g. in case you use sessions) 
-res.setHeader('Access-Control-Allow-Credentials', true); 
-// Pass to next layer of middleware 
-next(); 
-});
-//get call
-  app.get('/getData', getBingData.getBingData);
-  app.get('/getHistoricalData', getHistoricalData.getHistoricaldata )
-  app.get('/getNews', getBingData.getNews)
+//Add Router
+app.use(router);
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("listening"));
---
+++ controllers/getGovData.js
@@ -0,0 +1,13 @@
+const puppeteer = require("puppeteer");
+exports.getBingData = getBingData;
+
+async function getGovData(req, res, next) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://www.mohfw.gov.in/", { waitUntil: "networkidle0" });
+  const totalCaseScanned = await page.evaluate(
+    () => document.querySelector(".icount").innerText
+  );
+  res.send(totalCaseScanned);
+  browser.close();
+}
